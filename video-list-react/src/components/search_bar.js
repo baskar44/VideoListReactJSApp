c40a2a005cb62229610f5d19a977b039c7673d2b
baskar44 <baskg99@gmail.com>
2018-07-30 20:59:59 +1000

    Added throttling ofsearch using lodash

--- video-list-react/src/components/search_bar.js
+++ video-list-react/src/components/search_bar.js
@@ -11,6 +11,9 @@ class SearchBar extends Component {
     }
 
     render() {
+
+
+        
         return (
             <div className="search-bar">
                 <input 
--- video-list-react/src/components/video_detail.js
+++ video-list-react/src/components/video_detail.js
@@ -15,8 +15,8 @@ const VideoDetail = ({video}) => {
             <iframe className="embed-responsive-item" src={url}></iframe>
 
              <div className="details">
-                <div>{video.snippet.title}</div>
-                <div>{video.snippet.description}</div>
+                <h3>{video.snippet.title}</h3>
+                <p>{video.snippet.description}</p>
              </div>
         </div>
     )
